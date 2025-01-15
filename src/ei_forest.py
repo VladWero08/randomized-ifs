b2751178ce2b0b08cf93064ddc0064332dc4d6ee
VladWero08 <vlad_wero@yahoo.com>
2025-01-15 14:32:29 +0200

    added extended isolation forest algorithm

---
+++ src/ei_forest.py
@@ -0,0 +1,227 @@
+import typing as t
+import numpy as np
+
+from multiprocessing import Pool
+
+
+class ExternalNode:
+    def __init__(self, size: int, data):
+        self.size = size 
+        self.data = data
+
+
+class InternalNode:
+    def __init__(
+        self, 
+        left: "ExternalNode | InternalNode", 
+        right: "ExternalNode | InternalNode",
+        split_slope: np.ndarray,
+        split_intercept: np.ndarray,
+    ):
+        self.left = left
+        self.right = right
+        self.split_slope = split_slope
+        self.split_intercept = split_intercept
+
+
+class EITree:
+    def __init__(
+        self, 
+        height: int, 
+        height_limit: int,
+        extension_level: int = 0,    
+    ):
+        self.height = height
+        self.height_limit = height_limit
+        self.extension_level = extension_level
+        self.root: InternalNode | ExternalNode = None
+
+    def fit(self, X: np.ndarray) -> InternalNode | ExternalNode:
+        """
+        Fits a extended isolation tree for the given data.
+
+        Parameters:
+        -----------
+        X: np.ndarray
+            Data that needs to be fit.
+        """
+        if self.height >= self.height_limit or X.shape[0] <= 1:
+            self.root = ExternalNode(size=X.shape[0], data=X)
+            return self.root
+        
+        # randomly choose the split slope
+        idxs = np.random.choice(range(X.shape[1]), X.shape[1] - self.extension_level - 1, replace=False)
+        split_slope = np.random.normal(loc=0, scale=1, size=X.shape[1])
+        split_slope[idxs] = 0
+        # randomly choose the split intercept
+        split_intercept = np.random.uniform(X.min(axis=0), X.max(axis=0))
+        # compute the branching criteria
+        split_criteria = np.dot(X - split_intercept, split_slope)
+
+        # compute the split of the data
+        X_left = X[split_criteria < 0] 
+        X_right = X[split_criteria >= 0]
+
+        # compute the left and right side of the tree
+        node_left = EITree(self.height + 1, self.height_limit).fit(X_left)
+        node_right = EITree(self.height + 1, self.height_limit).fit(X_right)
+
+        self.root = InternalNode(node_left, node_right, split_slope, split_intercept, self.extension_level)
+        return self.root
+
+
+class EIForest:
+    def __init__(
+        self, 
+        n_trees: int = 100, 
+        sub_sample_size: int = 256, 
+        height_limit: t.Optional[int] = None,
+        n_processes: int = 8
+    ):
+        self.n_trees = n_trees
+        self.sub_sample_size = sub_sample_size
+        self.n_processes = n_processes
+        
+        self.height_limit: int = height_limit if height_limit else np.ceil(np.log2(self.sub_sample_size))
+        self.expected_depth: float = self.c(sub_sample_size)
+        self.ei_trees: t.List[EITree] = []
+
+    def c(self, size: int) -> float:
+        """
+        Sets the expected depth of a EITree 
+        based on the number of sub-samples.
+
+        Parameters:
+        -----------
+        size: int
+            The number of instances for each the unsuccessful 
+            search for a BST is computed on. 
+        """        
+        if size > 2:
+            H = np.log(size) + 0.5772156649
+            return 2 * H * (size - 1) - 2 * (size - 1) / size
+
+        if size == 2:
+            return 1
+
+        return 0.0
+
+    def fit_ei_tree(self, _) -> t.Optional[EITree]:
+        """
+        Fits a single EITree, assuming the data set 
+        was already defined in the EIForest object.
+        """
+        if self.X is None:
+            return
+        
+        indexes = np.random.choice(range(0, self.X.shape[0]), size=self.sub_sample_size, replace=False)
+        X_sub = self.X[indexes]
+
+        ei_tree = EITree(height=0, height_limit=self.height_limit)
+        ei_tree.fit(X_sub)
+
+        return ei_tree
+
+    def fit(self, X: np.ndarray) -> None:
+        """
+        Fits an ensemble of isolation trees for the given data.
+
+        Parameters:
+        -----------
+        X: np.ndarray
+            Data that needs to be fit.        
+        """
+        self.X = X
+
+        # use a pool to compute the trees in parallel
+        with Pool(processes=self.n_processes) as pool:
+            # assign the EITree training to the pool
+            ei_trees = pool.map(self.fit_ei_tree, range(self.n_trees))
+        
+        self.ei_trees = ei_trees 
+
+    def path_length(self, x: np.ndarray, ei_tree: EITree) -> float:
+        """
+        Computes the path length for a given sample in the given ITree.
+
+        The path value starts from 0. After each movement
+        from a node to another, the path is incremented.
+
+        After reaching an external node (leaf), the path
+        calculated until that point is returned plus an adjustment
+        for the subtree that was not continued beyond the height limit.
+        """
+        node = ei_tree.root
+        path = 0
+
+        while not isinstance(node, ExternalNode):
+            y = np.inner(x - node.split_intercept, node.split_slope)
+
+            if y < 0:
+                node = node.left
+            else:
+                node = node.right
+
+            path += 1
+
+        return path + self.c(node.size)
+
+    def avg_path_length(self, x: np.ndarray) -> float:
+        """
+        Computes the average path length for a single sample
+        among all trained isolation trees.
+        """
+        path_lengths = np.array([self.path_length(x, ei_tree) for ei_tree in self.ei_trees])
+        return np.mean(path_lengths)    
+    
+    def score(self, x: np.ndarray) -> float:
+        """
+        Computes the score for a single sample.
+        """
+        return np.power(2, -1 * self.avg_path_length(x) / self.expected_depth)
+
+    def scores(self, X: np.ndarray) -> np.ndarray:
+        """
+        Computes the score for all dataset with multiple samples.
+        """
+        scores = np.array([self.score(x) for x in X])
+        return scores
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import time 
+    from pyod.utils.data import generate_data_clusters
+
+    contamination = 0.1
+    X_train, X_test, y_train, y_test = generate_data_clusters(n_train=1000, n_test=200, n_clusters=2, n_features=2, contamination=contamination)
+    
+    sciforest = EIForest(n_trees=100)
+    start = time.time()
+    sciforest.fit(X_train)
+    end = time.time()
+    print(f"Time: {end - start}s")
+
+    X_train_scores = sciforest.scores(X_train)
+    X_test_scores = sciforest.scores(X_test)
+    
+    threshold = np.quantile(X_train_scores, 1 - contamination)
+    
+    X_train_preds = np.array([int(label) for label in (X_train_scores > threshold)])
+    X_test_preds = np.array([int(label) for label in (X_test_scores > threshold)])
+
+    print(f"Train ACC: {np.mean(X_train_preds == y_train)}")
+    print(f"Test ACC: {np.mean(X_test_preds == y_test)}")
+
+    fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+    axs[0].scatter(X_train[y_train == 0][:, 0], X_train[y_train == 0][:, 1], color="blue", label="normal")
+    axs[0].scatter(X_train[y_train == 1][:, 0], X_train[y_train == 1][:, 1], color="red", label="anomaly")
+    axs[0].set_title("Ground truth for train")
+    axs[0].legend()
+
+    axs[1].scatter(X_train[X_train_preds == 0][:, 0], X_train[X_train_preds == 0][:, 1], color="blue", label="normal")
+    axs[1].scatter(X_train[X_train_preds == 1][:, 0], X_train[X_train_preds == 1][:, 1], color="red", label="anomaly")
+    axs[1].set_title("Predictions for train")
+    axs[1].legend()
+
+    plt.show()
